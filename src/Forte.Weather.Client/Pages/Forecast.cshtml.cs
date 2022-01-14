3800b316dc229ea4d215eac3872a14d231382981
Nina Marie Wahl <nina.marie.wahl@gmail.com>
2022-01-14 09:20:55 +0100

    Forecast

---
+++ src/Forte.Weather.Client/Pages/Forecast.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Forte.Weather.Client.Pages
+{
+    public class ForecastModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
