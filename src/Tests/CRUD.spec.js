0308b58e6d059376cd66c4b71f8c87da90ad933d
Nina Marie Wahl <nina.marie.wahl@gmail.com>
2022-01-19 10:39:27 +0100

    delete test finished

--- src/Tests/CRUD.spec.js
+++ src/Tests/CRUD.spec.js
@@ -4,19 +4,28 @@ const { chromium } =  require('playwright')
 test('delete-location', async ({ }) => {
   const browser = await chromium.launch({ headless: false, slowMo: 500 }); 
   const page = await browser.newPage();
-  // Go to https://localhost:7276/
+  
   await page.goto('https://localhost:7276/');
-  await page.locator('button.delete').nth(0).click();
-    page.once('dialog', dialog => {
-        console.log(`Dialog message: ${dialog.message()}`);
-        dialog.dismiss().catch(() => {});
-    });
-    await expect(page).toHaveURL('https://localhost:7276/Locations');
 
-    // VERIFY THAT THE CORRECT ROW IS DELETED
+  await page.click('input[name="Input.Name"]');
+  await page.fill('input[name="Input.Name"]', 'Kristiansund');
+  await page.click('input[name="Input.Latitude"]');
+  await page.fill('input[name="Input.Latitude"]', '60');
+  await page.click('input[name="Input.Longitude"]');
+  await page.fill('input[name="Input.Longitude"]', '11');
+  await page.click('text=Submit');
+  await expect(page).toHaveURL('https://localhost:7276/Locations');
 
+  page.on('dialog', async dialog => {
+    await dialog.accept();
+  });
+  await page.click('tr:has(td:text("Kristiansund")) button.delete')
+  await expect(page).toHaveURL('https://localhost:7276/Locations');
+
+  await expect(page.locator('tr:has(td:text("Kristiansund"))')).toHaveCount(0) 
 
 });
+
 
 test('edit-location', async ({ }) => {
     const browser = await chromium.launch({ headless: false, slowMo: 500 }); 
